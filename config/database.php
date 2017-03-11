840818d6906c2e8795420a2c352c0cb03f307fef
mike <utnmike@gmail.com>
2017-03-11 16:24:41 -0600

    credentials bd for heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_e90360ab61f83b9'),
+            'username' => env('DB_USERNAME', 'b41ed286653b4f'),
+            'password' => env('DB_PASSWORD', '5d4046df'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
